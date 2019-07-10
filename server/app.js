d3959cbb3338507ab5e1d0b07109aab0c6c906fe
Sam wu <sam19970416@gmail.com>
2019-07-10 23:34:00 +0800

    update : add some sample code to import vue

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var apiRouter = require('./api/apis');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -18,9 +19,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// visit static resource
+//app.use(express.static(path.resolve(__dirname, '../dist')));
+// visit spa
+// app.get('*', function (req, res) {
+//   var html = fs.readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf-8');
+//   res.send(html);
+// });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/',indexRouter);
+app.use('/apis', apiRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
