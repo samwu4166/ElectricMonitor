b2965312618e4e365af17188204af1fea29b5549
Sam <sam19970416@gmail.com>
2019-07-12 02:10:40 +0800

    update : temp user api

--- server/api/member/user.js
+++ server/api/member/user.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+var conn = require('../connect');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Welcome users !');
+  let db = conn.emit(false, 'clientdb');
+  db
+   .tableList()
+   .then(data => {
+    res.send(data);
+   })
+   .catch(err => {
+     console.log(err);
+   })
 });
 
 module.exports = router;
