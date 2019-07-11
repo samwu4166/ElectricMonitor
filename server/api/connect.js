2b750581cb41356a69320de119d0aa30f2903be4
Sam <sam19970416@gmail.com>
2019-07-12 02:09:57 +0800

    update : add sql connection object

---
+++ server/api/connect.js
@@ -0,0 +1,13 @@
+let Mysqli = require('mysqli')
+
+//传入json
+let conn = new Mysqli({
+  host: 'localhost', // IP/域名
+  post: 3306, //端口， 默认 3306
+  user: 'app', //用户名
+  passwd: '1qaz2wsx', //密码
+  charset: '', // 数据库编码，默认 utf8 【可选】
+  db: '' // 可指定数据库，也可以不指定 【可选】
+})
+
+module.exports = conn;
