95976faa3e0de7d52dbd6eaa82e3806d2dfb9acf
Sam <sam19970416@gmail.com>
2019-07-12 02:09:46 +0800

    update : add point api to use

--- server/api/apis.js
+++ server/api/apis.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var api_router = express.Router();
 var userRouter = require('./member/user');
+var dataRouter = require('./point/data');
 
 api_router.use('/member',userRouter);
+api_router.use('/point',dataRouter);
 //api_router.use('/login');
 
 module.exports = api_router;
