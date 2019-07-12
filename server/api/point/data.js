a4c6aa332eac7caba284e58d27bd935d713b413b
Sam wu <sam19970416@gmail.com>
2019-07-12 17:23:57 +0800

    update : process datetime with momentjs

--- server/api/point/data.js
+++ server/api/point/data.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var conn = require('../connect');
+var moment = require('moment');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let point_nums = 9;
   let db = conn.emit(false, 'clientdb');
   db
-   .query(`select * from point_info ORDER BY point_info.datetime DESC limit ${[point_nums]}`)
+   .query(`select t.* from (select * from point_info ORDER BY point_info.datetime DESC limit ${[point_nums]})t order by t.tagname`)
    .then(data => {
+    data.map(function(row){
+       let new_date = moment(row['datetime']).format('YYYY-MM-DDTHH:mm:ss');
+       row['datetime'] = new_date;
+    })
     res.send(data);
    })
    .catch(err => {
