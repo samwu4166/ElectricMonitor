a1f5200a0df52a9a2aed74681ab13937df5e8eee
Sam <sam19970416@gmail.com>
2019-07-12 12:33:32 +0800

    update : let express be server not http

--- server/app.js
+++ server/app.js
@@ -4,16 +4,14 @@ var path = require('path');
 var fs = require('fs');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var apiRouter = require('./api/apis');
 var indexRouter = require('./routes/index');
 
 var app = express();
-
+var port = 8080;
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
+//swap jade for ejs etc
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,13 +20,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 // visit static resource
 //app.use(express.static(path.resolve(__dirname, '../dist')));
 // visit spa
-// app.get('*', function (req, res) {
-//   var html = fs.readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf-8');
-//   res.send(html);
-// });
-
-app.use('/',indexRouter);
 app.use('/api', apiRouter);
+app.get('*', function (req, res) {
+//   var html = fs.readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf-8');
+     var html = fs.readFileSync(path.resolve(__dirname, '../client/public/index.html'), 'utf-8');
+     res.send(html);
+ });
+
 
 
 
@@ -45,7 +43,11 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');
+});
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
 });
 
 module.exports = app;
