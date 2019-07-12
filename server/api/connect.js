ceb2b14bc50e5b8e6e7ea305b2df192a4fb82a30
Sam wu <sam19970416@gmail.com>
2019-07-12 17:23:45 +0800

    update : add conn2 to running on mac

--- server/api/connect.js
+++ server/api/connect.js
@@ -10,4 +10,13 @@ let conn = new Mysqli({
   db: '' // 可指定数据库，也可以不指定 【可选】
 })
 
-module.exports = conn;
+let conn2 = new Mysqli({
+  host: 'localhost', // IP/域名
+  post: 3306, //端口， 默认 3306
+  user: 'root', //用户名
+  passwd: 'j6t/6bj6', //密码
+  charset: '', // 数据库编码，默认 utf8 【可选】
+  db: '' // 可指定数据库，也可以不指定 【可选】
+})
+
+module.exports = conn2;
