4fccad419f767b9fe476a19064b0aa33c04288a0
writerwriter <catchingfire80@gmail.com>
2019-07-31 04:59:52 +0800

    Modify: add auth headers

--- client/src/api.js
+++ client/src/api.js
@@ -1,12 +1,21 @@
 let axios = require('axios');
+import store from './store'
 
-const dataRequest = axios.create({
-    baseURL: 'http://172.20.10.2:8080'
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:4000',
+    params: {}
     //172.20.10.2
 });
 
 //real-time data request
-export const apiDataRequest = () => dataRequest.get('/api/point/mssql');
-
+export const apiDataRequest = token => {
+    axiosInstance.defaults.headers.common['Authorization'] = token;
+    return axiosInstance.get('/result');
+}
+///api/point/mssql
 //login auth
-export const apiLoginAuth = () => dataRequest.post('/loginAuth', this.$store.getters.getLoginState);
+export const apiLoginAuth = () => axiosInstance.post('/auth/login', store.getters.getUser);
+export const apiLogout = token => {
+    axiosInstance.defaults.headers.common['Authorization'] = token;
+    return axiosInstance.post('/logout');
+}
