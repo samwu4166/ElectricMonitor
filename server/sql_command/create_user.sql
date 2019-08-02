45a38ced1265ca8619484ef63509530886944319
Sam wu <sam19970416@gmail.com>
2019-08-02 18:49:38 +0800

    update : sel constraint on create user

--- server/sql_command/create_user.sql
+++ server/sql_command/create_user.sql
@@ -1,1 +1,1 @@
-create table user_info (uuid varchar(256) Not null UNIQUE,account varchar(256) Not null UNIQUE,password varchar(256) Not null );
+create table user_info (uuid varchar(256) Not null UNIQUE,account varchar(256) Not null UNIQUE,password varchar(256) Not null,token varchar(256) Not null,foreign key (token) references authtoken(token) on update cascade on delete cascade);
