7fa79b25aaf7877d7f4b3bc6f44310d2aaeca43d
unknown <sam19970416@gmail.com>
2019-09-04 23:20:03 +0800

    update : add patch into cors header

--- server/src/index.js
+++ server/src/index.js
@@ -16,13 +16,13 @@ app.use(cookieParser());
 app.use(function(req, res, next) {
   console.log("get request : "+req.ip);
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 app.options("/*", function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   res.sendStatus(200);
 });
