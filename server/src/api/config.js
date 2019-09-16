c6cd55cab55d9f7d63382981310107fe2abcdd39
unknown <sam19970416@gmail.com>
2019-09-16 11:51:03 +0800

    update : use .env file to define system params

--- server/src/api/config.js
+++ server/src/api/config.js
@@ -1,36 +1,21 @@
 // Create connection to database
 var config = {
-    server: 'localhost',
+    server: process.env.DB_HOST,
     authentication: {
-        type: 'default',
+        type: process.env.DB_AUTH_TYPE,
         options: {
-            userName: 'SA', // update me
-            password: 'Samj6t/6bj6' // update me
+            userName: process.env.DB_USER, // update me
+            password: process.env.DB_PASS // update me
         }
     },
     options: {
-        port : 1401,  //because it is docker config
-        database: 'testDB',
+        port : process.env.DB_PORT,  //because it is docker config
+        database: process.env.DB_DATABASE,
         rowCollectionOnRequestCompletion: true,
     }
   }
-// var config = {
-//     server: '192.168.50.45',
-//     authentication: {
-//         type: 'default',
-//         options: {
-//             userName: 'app', // update me
-//             password: '1qaz2wsx' // update me
-//         }
-//     },
-//     options: {
-//         port : 1433, // real port on formal env
-//         database: 'app_project',
-//         rowCollectionOnRequestCompletion: true,
-//     }  
-// }
-var private_key = 'BD78B9F28AF21922BECAC57EBCC3F';
-var token_expire = 60*5;// test: 60mins ,formal : 5 mins
+var private_key = process.env.PRIVATE_KEY;
+var token_expire = process.env.TOKEN_EXP;// test: 60mins ,formal : 5 mins
 
 module.exports = {
     config,
